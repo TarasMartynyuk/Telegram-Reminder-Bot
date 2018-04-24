d7048b882495ba3d124c424a6f119cc7af496d8c
TarasMartynyuk <tarasmartynyuk22@gmail.com>
2018-04-24 18:32:14 +0300

    finished users

--- database/dbtest/test.py
+++ database/dbtest/test.py
@@ -2,13 +2,16 @@ from .dbprint import print_all_users
 import database.users as us
 from database.utils import get_users_col
 
-
 def test():
-    get_users_col().remove({})
-    us.add_new_user('Taras')
-    us.get_user('Taras')
-    print_all_users()
+    users_test()
 
+def users_test():
+    us.init()
+    # get_users_col().remove({})
+    # us.add_new_user('Taras')
+    # us.get_user('Taras')
+
+    print_all_users()
 
 
 
--- database/users.py
+++ database/users.py
@@ -2,12 +2,29 @@ from .utils import DatabaseConsts as dc, get_users_col
 from pymongo import MongoClient
 from pymongo.collection import Collection
 
+# class UserAlreadyExistsError:
+#         def __init__(self, message):
+#             self.message = message
+
+#         def __str__(self):
+#             return repr(self.message)
+
+
+def init():
+    '''
+    run this before using this module any further
+    adds the uniqueness constraint on users field
+    '''
+    get_users_col().create_index( 'user_name', unique = True)
+
+
 def add_new_user(user_name):
     '''
     adds a new user doc {
         name : user_name,
         trackables : [empty]
     } to db
+    raises DuplicateKeyError if user with user_name already exists
     '''
     # assert isinstance(dc.USERS_COLL_NAME, MongoClient)
 
@@ -27,21 +44,24 @@ def get_user(user_name):
         name : 'string',
         trackables : [string arr]
     }
-    user with that name must be present in collection
+    returns None if no doc with user_name is present in db
     '''
+
     users_coll = get_users_col()
-
-    assert users_coll.find({'name' : user_name}).count() == 1
-
     return users_coll.find_one({'name' : user_name})
 
 
 def user_registered(user_name):
     '''
-    returns whether the user with username is registered in db
+    returns true if 1 or more user docs with user_name are present in db
     '''
+    assert _users_with_name(user_name) <= 1
 
-    pass
+    return _users_with_name(user_name) == 1
+
+
+def _users_with_name(user_name):
+    return get_users_col().find({'name' : user_name}).count()
 
 
 
